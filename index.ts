920b1f499e60b9d30935f61bedce2e265d246dcd
Ava Venturino <ava.venturino@mths.ca>
2024-04-15 13:41:20 +0000

    added stuff

---
+++ index.ts
@@ -0,0 +1,24 @@
+/**
+ * Class that defines airplane speed.
+ *
+ * @author Ava Venturino
+ * @version 1.0
+ * @since 2024-04-09
+ */
+
+function reverseString(str: string): string {
+  if (str === "") {
+    return str;
+  } else {
+                                                return reverseString(str.substr(1)) + str[0];
+                                                    }
+}
+
+function main(): void {
+            const aString: string = "recursion";
+
+                console.log(`The original string is: ${aString}`);
+                    console.log(`The reverse string is: ${reverseString(aString)}`);
+
+                        console.log("\nDone.");
+}
