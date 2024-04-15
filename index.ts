1596db3b61e9be5748a9ee620471779cd2db945f
Ava Venturino <ava.venturino>
2024-04-15 10:02:24 -0400

    changed stuff

--- index.ts
+++ index.ts
@@ -6,19 +6,21 @@
  * @since 2024-04-09
  */
 
+import { createPrompt } from 'bun-promptx'
+
 function reverseString(str: string): string {
   if (str === "") {
-    return str;
+    return str
   } else {
-                                                return reverseString(str.substr(1)) + str[0];
-                                                    }
+    return reverseString(str.substr(1)) + str[0]
+  }
 }
 
 function main(): void {
-            const aString: string = "recursion";
+  const aString: string = "recursion"
 
-                console.log(`The original string is: ${aString}`);
-                    console.log(`The reverse string is: ${reverseString(aString)}`);
+  console.log(`The original string is: ${aString}`);
+  console.log(`The reverse string is: ${reverseString(aString)}`);
 
-                        console.log("\nDone.");
+  console.log("\nDone.");
 }
